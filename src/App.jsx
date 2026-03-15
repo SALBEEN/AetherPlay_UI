9a1da06e436bd3ff06fbd21b6d46f2d6e9e2c82f
salbeen chapagain <wizsalbeen69@gmail.com>
2026-03-15 21:24:23 +0545

    AetherPlay Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: "#1a1a1a",
+            color: "#f1f1f1",
+            border: "1px solid #2e2e2e",
+          },
+        }}
+      />
       <AppRoutes />
     </BrowserRouter>
   );
