9f07dae24fe39cbc2e367dd3ac5f2e7f91622e17
salbeen chapagain <wizsalbeen69@gmail.com>
2026-03-17 19:28:21 +0545

    Working on final debugging

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json", 
   },
 });
 
