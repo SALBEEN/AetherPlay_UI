8c1f8e4169375bbc99278debefd7ac1832293d61
salbeen chapagain <wizsalbeen69@gmail.com>
2026-04-24 14:04:27 +0545

    fix production api base url-

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "https://aetherplay-ui.onrender.com/api/v1",
+    import.meta.env.VITE_API_URL || "https://aetherplay-ui.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
